cd317c8cd433093fe6a22a9ae16a324db257b3ce
Elizabeth Arnold <eroxarnmc@gmail.com>
2022-04-19 01:55:27 -0700

    Speed up after 30 sec, controls for 2 player

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 /* MODDED CHANGES:
 
-track and display high score (5)
+track and display high score (5) DONE
+speed increase 30 sec into the game (5) DONE
+simultaneous 2 player (30) 
+smaller, faster ship (20) 
+alternating 2 player mode (20)
+display time remaining on the screen (10)
+randomize ship movement (5)
+add BGM (5)
 
 */
 
@@ -18,5 +25,6 @@ let borderUISize = game.config.height /15;
 let borderPadding = borderUISize /3;
 
 //reserve keyboard vars
-let keyF, keyR, keyLEFT, keyRIGHT;
+let keyW, keyA, keyD, keyR, keyLEFT, keyRIGHT, keyUP;
+//high score
 let highScore = 0;
